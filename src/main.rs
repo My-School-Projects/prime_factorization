cb9b2923631887cc54f33d30f4be1f9eabb2cfb1
Michael Dorst <code@mikefdorst.com>
2017-02-08 13:54:33 -0800

    Fix it up.

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,55 @@
 
 fn main() {
-
+    let n = 13195;
+    println!("The factors of {} are {:?}", n, prime_factors(n));
+    let n = 600851475143;
+    println!("The factors of {} are {:?}", n, prime_factors(n));
 }
 
-fn find_prime_factors(n: u64) -> Vec<u64>
-{
-    let v = find_factors(n);
-
-    let done = false;
-
-    while !done {
-        for i in v {
-            if !is_prime(i) {
-                let v2 = find_factors(i);
-                // replace i with the elements of v2
-            }
-        }
-    }
-}
-
-fn find_factors(n: u64) -> Vec<u64>
-{
-    let mut v = vec![];
-
-    let mut f = 1;
-
-    loop {
-        match find_smallest_factor(n, f+1) {
-            Some(x) => {
-                v.push(x);
-                if n / x != x {
-                    v.push(n / x);
-                }
-                f = x;
-            },
-            None => return v
-        }
-    }
-}
-
-/// Finds the smallest factor of n that is greater than i.
-fn find_smallest_factor(n: u64, i: u64) -> Option<u64>
+fn prime_factors(n: u64) -> Vec<u64>
 {
     let n = n as f64;
     let root_n = n.sqrt() as u64;
 
-    for k in i..root_n+1 {
-        if is_integer(n / k as f64) {
-            return Some(k);
+    let mut v = vec![];
+
+    for j in 2..root_n+1 {
+        if is_integer(n / j as f64) {
+            // j is a factor of n.
+            // Find the prime factors of j (recursively), and append them to v.
+            // If there are none (j is prime), append j.
+            let factors = prime_factors(j);
+            if factors.len() == 0 {
+                if !v.contains(&j) {
+                    v.push(j);
+                }
+            } else {
+                for i in factors {
+                    if !v.contains(&i) {
+                        v.push(i);
+                    }
+                }
+            }
+            // For every j < sqrt(n), there is another factor of n, k = n / j
+            // We do the same thing for k.
+            let k = n as u64/ j;
+            if k != j {
+                let factors = prime_factors(k);
+                if factors.len() == 0 {
+                    if !v.contains(&k) {
+                        v.push(k);
+                    }
+                } else {
+                    for i in factors {
+                        if !v.contains(&i) {
+                            v.push(i);
+                        }
+                    }
+                }
+            }
         }
     }
-    None
-}
-
-fn is_prime(n: u64) -> bool
-{
-    match find_smallest_factor(n, 2) {
-        Some(_) => false,
-        None => true
-    }
+    v
 }
 
 fn is_integer(n: f64) -> bool
